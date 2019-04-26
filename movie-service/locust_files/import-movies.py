1c77430d6afc90efacc3024856b6945669cfc440
Kevin Liu <kliu@eventbrite.com>
2019-04-25 17:01:18 -0700

    adding csv file

--- movie-service/locust_files/import-movies.py
+++ movie-service/locust_files/import-movies.py
@@ -1,13 +1,19 @@
 from locust import Locust, TaskSet, task
+import csv
 
 def login(l):
     l.client.post("/api/v1/login/", {"username":"locust", "password":"locust"})
 
 class MovieLoader(TaskSet):
 
+    def load_movies_from_csv():
+        with open('movies.csv') as csvfile:
+            return csv.DictReader()
+
+    movies_dict = load_movies_from_csv()
+
     @task(1)
     def process_movie_csv(self):
-        self.login()
-        # read CSV file
-        # generate movie
+        
+    
 
